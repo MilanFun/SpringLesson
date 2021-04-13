1c5b68de8befc08be43a985c9e1f162cd76efbb4
Aleksey Ptukha <ptukha.aiu@phystech.edu>
2021-04-13 14:32:43 +0300

    @RequestParam, HttpServletRequest, ref to page with parametres

--- SpringMvcFirstApp/src/main/java/org/example/config/controllers/FirstController.java
+++ SpringMvcFirstApp/src/main/java/org/example/config/controllers/FirstController.java
@@ -3,12 +3,28 @@ package org.example.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+//  @GetMapping("/hello")
+//  public String halloPage(HttpServletRequest request) {
+//    String name = request.getParameter("name");
+//    String surname = request.getParameter("surname");
+//
+//    System.out.println("Hello, " + name + " " + surname);
+//
+//    return "first/hello";
+//  }
+
   @GetMapping("/hello")
-  public String halloPage() {
+  public String halloPage(@RequestParam(value = "name", required = false) String name,
+                          @RequestParam(value = "surname", required = false) String surname) {
+    System.out.println("Hello, " + name + " " + surname);
+
     return "first/hello";
   }
 
